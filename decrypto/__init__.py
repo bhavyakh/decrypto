f502c00de91bd7c8e72da945d551a44982a7c19d
bhavyakh <khandelwalbhavya7@gmail.com>
2021-04-22 16:31:29 +0530

    Added submit route

--- decrypto/__init__.py
+++ decrypto/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template('base.html')
+
+
+@app.route("/submit", methods=['POST', 'GET'])
+def submit():
+
+    if request.method == 'GET':
+        user = request.args.get('cipher')
+        print(user)
+    return ("Hello world")
 
 
 if __name__ == '__main__':
