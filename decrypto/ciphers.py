288c33eca62aa576adf0319a1fe8b2a7ee2811de
bhavyakh <khandelwalbhavya7@gmail.com>
2021-04-25 20:08:04 +0530

    Changes to ciphers.py

--- decrypto/ciphers.py
+++ decrypto/ciphers.py
@@ -1,4 +1,5 @@
-from decrypto.cipher import (Atbash, Rot, RailFence, Basen, AsciiShift, Binary)
+from decrypto.cipher import (
+    Atbash, Rot, RailFence, Basen, AsciiShift, Binary, T9Text, DTMF, Periodic, Prime, A1Z26)
 import json
 from json2html import *
 
@@ -9,15 +10,24 @@ class Cipher():
         self.key = key
         # TODO: Bacon, keyboard
 
-    def decrypt(self, category=1):
+    def decrypt(self, category=2):
         if(category == 1):
             data = Atbash.decrypt(self.message)
             data.update(Rot.decrypt(self.message))
             data.update(RailFence.decrypt(self.message))
             data.update(Basen.decrypt(self.message))
             data.update(AsciiShift.decrypt(self.message))
+
+            print(data)
+            e = json2html.convert(json=json.dumps(data))
+        elif(category == 2):
+            data = T9Text.decrypt(self.message)
+            data.update(DTMF.decrypt(self.message))
             data.update(Binary.decrypt(self.message))
+            data.update(Periodic.decrypt(self.message))
+            data.update(Prime.decrypt(self.message))
+            data.update(A1Z26.decrypt(self.message))
             print(data)
             e = json2html.convert(json=json.dumps(data))
 
-            return e
+        return e
